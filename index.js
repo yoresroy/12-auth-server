739604cf7ccfb3f0875afbad60b01bb40a8fb785
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2023-04-24 15:08:28 -0600

    Add - angular login

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
+
 const { dbConnnection } = require('./db/config');
 require('dotenv').config();
 
@@ -22,6 +25,10 @@ app.use( express.json() );
 //Rutas
 app.use( '/api/auth', require('./routes/auth') );
 
+//Manejar demás rutas
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen( process.env.PORT, () => {
     console.log( `Servidor corriendo en puerto ${process.env.PORT}` );
